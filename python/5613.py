d0e11bac076743eb214af008dceddd5003cc2d4d
kanggeongu <kdr06006@naver.com>
2019-04-22 23:15:32 +0900

    4/22

--- python/5613.py
+++ python/5613.py
@@ -2,5 +2,7 @@ res = ""
 s = input()
 while(s != "="):
     res = res + s
+    if((s != "+") and (s != "-") and (s != "*") and (s != "/")):
+        res = int(eval(res))
     s = input()
 print(eval(res))
